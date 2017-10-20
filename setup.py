8956d43a100b5082040f3f76ab0002f7f4b4b836
Anindya Sundar Paul <anindya.paul@ipay.com.bd>
2017-10-20 19:19:37 +0600

    Reformate spacing in files

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
 setup(
-    name = "opendota",
-    version = "0.1.0",
-    description = "Thin python client for the OpenDota API",
-    url = "https://github.com/anindyaspaul/pyOpenDota",
-    author = "Anindya Sundar Paul",
-    author_email = "anindya@anindyaspaul.com",
-    license = "GPL-3.0",
-    packages = ["opendota"],
+    name="opendota",
+    version="0.1.0",
+    description="Thin python client for the OpenDota API",
+    url="https://github.com/anindyaspaul/pyOpenDota",
+    author="Anindya Sundar Paul",
+    author_email="anindya@anindyaspaul.com",
+    license="GPL-3.0",
+    packages=["opendota"],
 )
